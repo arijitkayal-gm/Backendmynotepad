ee0dfe03f63542d3d135933473afcfe3f4d42822
arijitkayal-gm <128666888+arijitkayal-gm@users.noreply.github.com>
2023-04-12 11:35:46 +0530

    First commit: server to mongo connection

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI).then(()=>{console.log("Connection  successful")}).catch((err)=>{console.log("Connection not successful")})
+}
+
+
+module.exports= connectToMongo;
